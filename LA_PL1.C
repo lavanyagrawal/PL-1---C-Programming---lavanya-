2064ae86c10c47884eded236a36ce8f7cfa391ce
lavanyagrawal <agrawallavanya555@gmail.com>
2026-09-02 21:48:04 +0530

    Add student information program in LA_PL1.C

---
+++ LA_PL1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum = 55;
+    per = 88.88;
+    grade = ‘A’;
+
+    printf(“ student Information \n  “);
+    printf(“\n Roll number :%d” , rollnum );
+    printf(“\n percentage : %f”, per);
+    printf(“\n Grade : %c”, grade);
+    return 0;
+}
